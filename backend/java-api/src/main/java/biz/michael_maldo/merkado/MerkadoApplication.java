e621d8b1448aa251fa593287b13b1f2dfd17f2f3
gnostex <gnosticka@gmail.com>
2026-05-23 22:40:43 +1000

    chore: bootstrap java-api Spring Boot application

---
+++ backend/java-api/src/main/java/biz/michael_maldo/merkado/MerkadoApplication.java
@@ -0,0 +1,13 @@
+package biz.michael_maldo.merkado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerkadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerkadoApplication.class, args);
+    }
+
+}
